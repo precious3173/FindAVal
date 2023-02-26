987aa420bcffc80474c23234762347ca6b63b691
Admin <preciouspeter3173@gmail.com>
2023-02-26 12:33:01 +0100

    sign up arraylist added

---
+++ app/src/main/java/com/example/findaval/Database/SignUPArray.java
@@ -0,0 +1,41 @@
+package com.example.findaval.Database;
+
+public class SignUPArray {
+
+    String fullName;
+    String email;
+    String phoneNumber;
+    String password;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
