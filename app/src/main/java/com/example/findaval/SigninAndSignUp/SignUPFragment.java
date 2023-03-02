835e092b890981245e6ad9d8cad66637e3eda0da
Admin <preciouspeter3173@gmail.com>
2023-03-02 22:48:32 +0100

    Sign Up and volley impelementation done

--- app/src/main/java/com/example/findaval/SigninAndSignUp/SignUPFragment.java
+++ app/src/main/java/com/example/findaval/SigninAndSignUp/SignUPFragment.java
@@ -1,6 +1,7 @@
 package com.example.findaval.SigninAndSignUp;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,14 +11,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.findaval.Constant;
 import com.example.findaval.R;
+import com.example.findaval.UI.MainActivity;
 import com.example.findaval.databinding.FragmentSignUPBinding;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SignUPFragment extends Fragment {
@@ -46,8 +55,8 @@ public class SignUPFragment extends Fragment {
       password = binding.password;
       email = binding.email;
 
-      if(fullName.getText().toString().isEmpty() && phoneNumber.getText().toString().isEmpty()
-      && password.getText().toString().isEmpty() && email.getText().toString().isEmpty()){
+      if(fullName.getText().toString().equals("") && phoneNumber.getText().toString().equals("")
+      && password.getText().toString().equals("") && email.getText().toString().equals("")){
 
       binding.signup.setBackgroundColor(R.color.grey);
       }
@@ -66,6 +75,15 @@ public class SignUPFragment extends Fragment {
                       @Override
                       public void onResponse(String response) {
 
+                          if(response.equals("successful")){
+                              Intent intent = new Intent(getActivity(), MainActivity.class);
+                              startActivity(intent);
+
+                          }
+                          else {
+
+                              Toast.makeText(getContext(), "Registration was unsuccessful", Toast.LENGTH_SHORT).show();
+                          }
                       }
                   }, new Response.ErrorListener() {
                       @Override
@@ -73,7 +91,23 @@ public class SignUPFragment extends Fragment {
 
                           error.printStackTrace();
                       }
-                  });
+                  }){
+                      @Override
+                      protected Map<String, String> getParams() throws AuthFailureError{
+
+                          Map<String, String>loginDetails = new HashMap<>();
+
+                          loginDetails.put("fullname", fullName.toString());
+                          loginDetails.put("email", email.toString());
+                          loginDetails.put("password", password.toString());
+                          loginDetails.put("phonenumber", phoneNumber.toString());
+
+                          return loginDetails;
+                      }
+                  };
+
+                  RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+                  requestQueue.add(stringRequest);
               }
           });
       }
